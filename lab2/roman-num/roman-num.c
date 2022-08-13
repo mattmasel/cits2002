e595be1f7bde8d273b64da9847a58cec43e8328e
mattmasel <20518121@student.uwa.edu.au>
2022-08-13 15:14:06 +0800

    completed task 10, roman-num.c. lab 2 finished except pangrammatic autograms question

--- lab2/roman-num/roman-num.c
+++ lab2/roman-num/roman-num.c
@@ -2,79 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Write a program that prints out all "Roman numeral" equivalents of the numbers
-   between 1 and the single argument supplied to the program.
-   Use the rules for writing Roman numerals, and the online calculator, from: 
-   Calculator Soup (just for the numerals I, V=5, X=10, L=50, C=100, D, M).            */
+/* Write a program that prints out all "Roman numeral" equivalents of any given number, only works up to M */
 
-int current_roman = 1000;
-char numeral[5] = "M";
-
-void roman(int number)
+void roman(char *number)
 {
-    int val = number;
-    while(val != 0) {
-        int diff = val / current_roman;
-        val -= (current_roman * diff);
-        for(int i = 0; i < diff; i++) {
-            printf("%s", numeral);
+    int num = atoi(number);
+    // arrays to hold numerals and their values 
+    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char *roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
+    // loop over number until it is zero
+    for(int i = 0; num != 0; i++) {
+        while(num >= val[i]) {
+            num -= val[i];
+            printf("%s", roman[i]);
         }
-        switch(current_roman) {
-            case 1000 :
-                current_roman = 500;
-                strcpy(numeral, "D");
-                break;
-            case 500 :
-                current_roman = 100;
-                strcpy(numeral, "C");
-                break;
-            case 100 :
-                current_roman = 50;
-                strcpy(numeral, "L");
-                break;
-            case 50 :
-                current_roman = 11;
-                strcpy(numeral, "X");
-                break;
-            case 11 :
-                current_roman = 10;
-                strcpy(numeral, "XI");
-                break;
-            case 10 :
-                current_roman = 9;
-                strcpy(numeral, "IX");
-                break;
-            case 9 :
-                current_roman = 8;
-                strcpy(numeral, "VIII");
-                break;
-            case 8 :
-                current_roman = 7;
-                strcpy(numeral, "VII");
-                break;
-            case 7 :
-                current_roman = 6;
-                strcpy(numeral, "VI");
-                break;
-            case 6 :
-                current_roman = 5;
-                strcpy(numeral, "V");
-                break;
-            case 5 :
-                current_roman = 4;
-                strcpy(numeral, "IV");
-                break;
-            case 4 :
-                current_roman = 1;
-                strcpy(numeral, "I");
-                break;
-            case 1 :
-                current_roman = 1000;
-                strcpy(numeral, "M");
-                break;
-        }
-        printf("\n");
     }
+    printf("\n");
 }
 
 int main(int argcount, char *argvalue[])
@@ -86,8 +30,7 @@ int main(int argcount, char *argvalue[])
     }
     else {
         for(int i = 1; i < argcount; i++) {
-            int number = atoi(argvalue[i]);
-            roman(number);
+            roman(argvalue[i]);
         }
     }
     return EXIT_SUCCESS;
