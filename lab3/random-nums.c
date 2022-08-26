fa99d4095c67b5691940c1de4e3e915bcde6d002
mattmasel <20518121@student.uwa.edu.au>
2022-08-26 19:46:01 +0800

    completed part c of random-nums.c

--- lab3/random-nums.c
+++ lab3/random-nums.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//  INSERTION SORT 
+int get_large(int *array)
+{
+    for(int i = 0; i < 9; i++) {
+        for(int j = i + 1; j < 10; j++) {
+            if(array[i] > array[j]) {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    return array[9];
+}
+
 int *randomize(int *array)
 {
     int i;
@@ -10,7 +25,7 @@ int *randomize(int *array)
 
     while(i < 10) {
 
-        array[i] = rand();
+        array[i] = rand() % 100;
         i++;
     }
     return array;
@@ -19,7 +34,7 @@ int *randomize(int *array)
 int main(int argc, char *argv[])
 {
 //  ENSURE THAT PROGRAM HAS CORRECT NUMBER OF ARGUMENTS
-    if (argc != 3) {
+    if (argc > 2) {
         fprintf(stderr, "Usage: %s <seed> <n>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -27,9 +42,13 @@ int main(int argc, char *argv[])
         int *random_numbers;
         int array[10]; 
         random_numbers = randomize(array);
+//  PRINT ALL NUMBERS
         for(int i = 0; i < 10; i++) {
-            printf("%i\n", random_numbers[i]);
+            printf("%i. %i\n", i + 1, random_numbers[i]);
         }
+//  PRINT LARGEST NUMBER
+        int largest = get_large(array);
+        printf("\nLargest number: %d\n", largest);
     
         exit(EXIT_SUCCESS);
     }
