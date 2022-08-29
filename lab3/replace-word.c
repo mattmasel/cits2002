9bf122368ae4b8c922eb7d8025f68203d4c7490f
mattmasel <20518121@student.uwa.edu.au>
2022-08-29 16:48:29 +0800

    q6, still working on solution, removes single word

--- lab3/replace-word.c
+++ lab3/replace-word.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+void replace(char new[], char old[], char source[])
+{
+
+    char *output;
+    char *result = source;
+    while (strstr(source, old) != NULL) {
+
+        output = strstr(result, old);
+        for(int i = 0; i < strlen(old); i++){
+            output[i] = '*';
+        }
+        printf("%s\n", output);
+
+
+        char *decay;
+        decay = strtok(output, "*");
+        result = strcat(new, decay);
+        printf("%s\n", result);
+        
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
 //  ENSURE THAT PROGRAM HAS CORRECT NUMBER OF ARGUMENTS
@@ -10,7 +33,11 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     else {
-            
+        printf("Input: %s\n", argv[3]);
+
+        replace(argv[1], argv[2], argv[3]);
+        printf("Output: %s\n", argv[3]);
+
 
         exit(EXIT_SUCCESS);
     }
