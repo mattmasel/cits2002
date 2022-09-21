853f1aef5e32611b2c5531a53e0665b09c5173bf
mattmasel <20518121@student.uwa.edu.au>
2022-09-21 19:09:56 +0800

    completed task 2 array-pointers.c

---
+++ lab4/array-pointers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int *maximum_a(int values[], int n)
+{
+    int max = INT_MIN;
+    int *pmax;
+
+    for(int i = 0; i < n; i++) {
+        if(values[i] > max) {
+            max = values[i];
+            pmax = &values[i];
+        }
+    }
+    
+    return pmax;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2) {
+        printf("Usage %s: <int-1>, <int-2>, ..., <int-n>", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    else {
+        
+        int n = argc - 1;
+        int values[n];
+        
+        //  COPY ARGS INTO ARRAY
+        for(int i = 0; i < n; i++) {
+            values[i] = atoi(argv[i + 1]);
+        }
+        
+        //  RETURNS POINTER TO MAX INT IN ARRAY
+        int *max_int = maximum_a(values, n);
+        
+        printf("Max int from array = %i\n", *max_int);
+
+        exit(EXIT_SUCCESS);
+    }
+    return 0;
+}
