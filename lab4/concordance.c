156f5f29c0e981077685c525f790114bda1d4a18
mattmasel <20518121@student.uwa.edu.au>
2022-10-04 18:01:28 +0800

    created main() for concordance.c

---
+++ lab4/concordance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+    if(argc < 2) {
+        printf("Usage: %s <str1> <str2>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    else {
+
+        exit(EXIT_SUCCESS);
+    }
+    return 0;
+}
