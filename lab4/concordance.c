73eb781824f05b5c4eba49e15026f744012373f9
mattmasel <20518121@student.uwa.edu.au>
2022-10-04 18:57:18 +0800

    added extract_file function scafolding

--- lab4/concordance.c
+++ lab4/concordance.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+void extract_file(char *file)
+{
+    ;
+}
+
 int main (int argc, char *argv[])
 {
-    if(argc < 2) {
-        printf("Usage: %s <str1> <str2>\n", argv[0]);
+    if(argc != 2) {
+        printf("Usage: %s <filepath>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     else {
-
+        char *file = argv[1];
+        extract_file(file);
         exit(EXIT_SUCCESS);
     }
     return 0;
